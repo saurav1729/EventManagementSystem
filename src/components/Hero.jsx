1a724b66e68063860fa1210b61672a4a88b56fff
Amaldeep <amaldeeppatra21@gmail.com>
2024-06-08 22:45:52 +0530

    homepage latest

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,18 +16,22 @@ const Hero = () => {
  
   return (
     <>
-      <div className='bg-[#000000] w-screen h-screen flex'>
-        <div className='w-[50%] h-full p-[10px] pt-10 flex flex-col items-center'>
-            <img className='w-[22rem] h-[17rem] mt-2 rounded-[45px] border-yellow-ochre border-[10px] mx-auto ' src={koruslogo}></img>
+      <div className='absolute w-screen h-screen top-20 bg-black opacity-85'>
+      </div>
+      <div className='w-screen h-screen flex'>
+        <div className='z-[1] w-[50%] h-full p-[10px] pt-16 flex flex-col items-center'>
+            <img className='w-[19rem] h-[17rem] mt-2 rounded-[45px] border-yellow-ochre border-[10px] mx-auto ' src={koruslogo}></img>
             <div className='flex flex-row justify-center gap-2'>
             <img className='w-[10rem] h-[10rem] mt-2 rounded-[45px] border-yellow-ochre border-[10px]' src={infinityLogo}></img>
             <img className='w-[10rem] h-[10rem] mt-2 rounded-[45px] border-yellow-ochre border-[10px]' src={pic2}></img>
             <img className='w-[10rem] h-[10rem] mt-2 rounded-[45px] border-yellow-ochre border-[10px]' src={klavier}></img>
             </div>
-            <div className='h-[5rem] rounded-[14px] w-[377px] bg-yellow-ochre mx-auto mt-[20px] flex justify-center items-center text-[2rem] text-[#ffffff] font-[400] '>book your slot</div>
+            {/* <div className='h-[5rem] rounded-[14px] w-[377px] bg-yellow-ochre mx-auto mt-[20px] flex justify-center items-center text-[2rem] text-[#ffffff] font-[400] font-freeman'>book your slot</div> */}
         </div>
-        <div className='text-[white] pt-[30px]'><h1 className='mt-[3.5rem] ml-[3rem] text-[3rem] font-[500] font-freeman'>Now book</h1>
-             <h1 className='ml-[3rem] text-[3rem] font-[500] font-freeman'>your slot in <span className='text-yellow-ochre'>{typeEffect}</span></h1>
+        <div className='z-[1] text-[white] pt-[54px] w-[550px]'>
+          <h1 className='mt-[3.5rem] ml-[3rem] text-[3rem] font-[500] font-freeman'>Now book</h1>
+          <h1 className='ml-[3rem] text-[3rem] font-[500] font-freeman'>your slot in <span className='text-yellow-ochre'>{typeEffect}</span></h1>
+          <div className='h-[5rem] rounded-[14px] w-[377px] bg-yellow-ochre mx-auto mt-[20px] flex justify-center items-center text-[2rem] text-[#ffffff] font-[400] font-freeman hover:bg-[#886800] hover:cursor-pointer'>book your slot</div>
         </div>
       </div>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,18 @@ import logo from '../resources/kiitlogo.svg'
 
 const Nav = () => {
   return (
-    <nav className='bg-black text-white flex justify-around w-screen'>
-        <div><img src={logo} alt="" /></div>
+    <>
+    <div className='absolute w-screen h-20 bg-black opacity-85'>
+    </div>
+    <nav className='text-white flex justify-around w-screen'>
+        <div className='z-[1]'><img src={logo} alt="" /></div>
         <div className='flex items-center'>
-            <div className='mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">Home</a></div>
-            <div className='mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">About</a></div>
-            <div className='mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">Socities</a></div>
+            <div className= 'z-[1] mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">Home</a></div>
+            <div className='z-[1] mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">About</a></div>
+            <div className='z-[1] mx-10 bg-yellow-ochre text-xl rounded-[25px] p-[10px] px-[20px] font-freeman hover:bg-[#CB9D0698] hover:scale-110'><a href="">Socities</a></div>
         </div>
     </nav>
+    </>
   )
 }
 
